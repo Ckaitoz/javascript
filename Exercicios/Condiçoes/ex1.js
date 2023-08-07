e5a7195be3c954104c6c518606e0b7356cca007b
Ckaitoz <caio.felipevs@gmail.com>
2023-08-07 17:28:13 -0300

    condiçoes

---
+++ Exercicios/Condiçoes/ex1.js
@@ -0,0 +1,18 @@
+// if(condição){
+//     true
+// } else {
+//     false
+// }
+
+// Condição simples {
+//     if(condição){
+//         true
+//     }
+// }
+console.log('o console esta funcionando')
+var vel = 70
+console.log(`a velocidade que você esta e ${vel} km/h`)
+if(vel > 60){
+    console.log('você utrapaçou a velocidade maxima. Multado!')
+}
+console.log('Se beber não dirija')
---
+++ Exercicios/Condiçoes/ex2.js
@@ -0,0 +1,3 @@
+var pais = 'brasil'
+if (pais == 'Brasil'){console.log (' voce e Brasileiro')}
+else { console.log (' voce e Estrangeiro')}
