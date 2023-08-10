33c5154e66c015c8b21c94f7cfff7e3954a77590
Ckaitoz <caio.felipevs@gmail.com>
2023-08-10 16:09:47 -0300

    aula

---
+++ Exercicios/repetiçoes/ambiente.js
@@ -0,0 +1,24 @@
+// var c = 1 
+// while(c <= 6){
+//     console.log('Tudo bem?')
+//     c++
+// }
+
+// | |
+//  V
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+
+// | |
+//  V
+
+var c = 1 
+do{
+    console.log('Tudo bem?')
+    c++
+}while (c <= 10)
